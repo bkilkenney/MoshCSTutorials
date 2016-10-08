cb16beb388d64d98f1647885b45e782f3bab2b1a
Brad Kilkenney <bkilkenney@gmail.com>
2016-10-08 00:43:44 -0400

    Initial Commit

---
+++ MoshCSTutorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoshCSTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cw tab tab is snippet for Console.WriteLine
+            Console.WriteLine();
+
+            //Ctrl X gets rid of line
+
+            
+            
+            //          ("{Place holders}", arguments)  -  This is a format string
+
+            Console.WriteLine("Ints go from {0} to {1}", int.MinValue, int.MaxValue );
+
+            int a = 1;
+            int b = 2;
+            Console.WriteLine(a == b); //Prints: False. Equality comparisons evaluate to a bool
+            Console.WriteLine(a != b);
+   
+        }
+    }
+}
